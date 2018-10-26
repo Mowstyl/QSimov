3d82340bca4919229e5696802406f7058005b26b
Paco <fjgramirez@abdprof.com>
2018-10-26 12:00:07 +0200

    Añadimos el fichero Qdeploy.sh

---
+++ Qdeploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Credenciales para conectarse al servidor en el que se despliega el contenedor;
+
+
+# Debe estar definido en Gitlab, como una variable del proyecto
+
+
+DEPLOYMENT_PASS=$1
+
+IP=quantum@10.0.0.240
+
+# Conectarse por SSH al servidor de builds, 
+
+sshpass -p "$DEPLOYMENT_PASS" ssh -o StrictHostKeyChecking=no -T "$IP" << EOF
+cd /home/quantum
+git pull -u origin master
+exit
+EOF
