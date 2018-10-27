60d36b382e5de87bedc8c3692ada51396feea10f
Hernán Indíbil de la Cruz Calvo <hernanindibil.lacruz@alu.uclm.es>
2018-10-27 13:30:03 +0200

    Now the folder gets completely restored

--- Qdeploy.sh
+++ Qdeploy.sh
@@ -13,7 +13,9 @@ IP=quantum@10.0.0.240
 # Conectarse por SSH al servidor de builds, 
 
 sshpass -p "$DEPLOYMENT_PASS" ssh -o StrictHostKeyChecking=no -T "$IP" << EOF
+mkdir -p /home/quantum/QSimulator
 cd /home/quantum/QSimulator
+rm -rf *
 git init
 git remote add origin http://10.0.0.250/quantum/QSimulator.git
 git pull -u origin master
