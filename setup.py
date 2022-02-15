a952fa5b471f46d634481825cecaddb9d389fedb
Hernán Indíbil de la Cruz Calvo <indikalesken@hotmail.com>
2022-02-15 22:25:06 +0100

    Fixed partial traces on new doki version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def main():
         install_requires=[
             "numpy>=1.21",
             "matplotlib>=3.5.1",
-            "doki-Mowstyl>=1.3.1"
+            "doki-Mowstyl>=1.3.2"
         ]
     )
 
