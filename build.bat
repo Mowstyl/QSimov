153f65b71dd21f85cf96f87e06aecafac26b77ea
Hernán Indíbil de la Cruz Calvo <indikalesken@hotmail.com>
2024-10-21 20:50:32 +0200

    Fixed sympy dep, updated scripts

--- build.bat
+++ build.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
-SET QSimovVersion=5.1.1
+SET QSimovVersion=5.1.3
 :: Removing old wheels
-del "dist\qsimov_Mowstyl-*.whl"
+del "dist\qsimov-*.whl"
 :: Removing old sources
-del "dist\qsimov-Mowstyl-*.tar.gz"
+del "dist\qsimov-*.tar.gz"
 
 :: Cleaning Python3.11
-py -3.11 -m pip uninstall qsimov_Mowstyl -y
+py -3.12 -m pip uninstall qsimov -y
 rmdir "qsimov_Mowstyl.egg-info" /S /Q
 :: Building Python3.11
-py -3.11 -m build
+py -3.12 -m build
 :: Installing Python3.11
-py -3.11 -m pip install --user dist/qsimov_Mowstyl-%QSimovVersion%-py3-none-any.whl
+py -3.12 -m pip install --user dist/qsimov-%QSimovVersion%-py3-none-any.whl
 
 pause
